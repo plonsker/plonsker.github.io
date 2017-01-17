f671b0bf8a84e5af097194377363fee9cec33b77
joe <joeplonsker@gmail.com>
2017-01-16 22:13:20 -0500

    add fadeToggle

--- public/js/application.js
+++ public/js/application.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
     $("#about-click").click(function(event){
         event.preventDefault();
-        $(".about-container").toggle();
+        $(".about-container").fadeToggle();
     });
-   
+
 });
 
 $(document).ready(function(){
     $("#software-click").click(function(event){
         event.preventDefault();
-        $("#software-container").toggle();
+        $("#software-container").fadeToggle();
     });
-   
+
 });
 
 $(document).ready(function(){
     $("#photography-click").click(function(event){
         event.preventDefault();
-        $("#photography-container").toggle();
+        $("#photography-container").fadeToggle();
          var flkty = new Flickity( '.main-gallery', {
           cellAlign: 'left',
           contain: true,
@@ -26,21 +26,21 @@ $(document).ready(function(){
           autoPlay: true
         });
     });
-   
+
 });
 
 
-$(document).ready(function(){ 
+$(document).ready(function(){
     $("#contact-click").click(function(event){
         event.preventDefault();
-        $(".contact-imgs").toggle();
+        $(".contact-imgs").fadeToggle();
         $("#contact-click").hide();
     });
   });
 
-$(document).ready(function(){ 
+$(document).ready(function(){
     $("#music-click").click(function(event){
         event.preventDefault();
-        $("#music-container").toggle();
+        $("#music-container").fadeToggle();
     });
   });
