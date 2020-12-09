284a315526d4669f1f817ecee35d2d4ee99aea39
plonsker <joeplonsker@gmail.com>
2020-12-09 16:19:55 -0500

    create first test gallery

--- public/js/category-gallery.js
+++ public/js/category-gallery.js
@@ -1,48 +1,96 @@
 /*Downloaded from https://www.codeseek.co/travisw/zillow-like-photo-gallery-MvmXdY */
-var startSlideshow = function() {
 
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+// $(function() {
+//
+//   $('.tile-lg').on('click', function() {
+//     var uniqueId = $(this).uniqueId();
+//     console.log(uniqueId);
+//   });
+//
+// });
 
-  // build items array
-  var items = [
-    {
-      src: 'https://s27.postimg.cc/g2so1q503/listing-0.jpg',
-      w: 1024,
-      h: 679
-    },
-    {
-      src: 'https://s28.postimg.cc/j7pcb7r25/listing-1.jpg',
-      w: 1024,
-      h: 679
-    },
-    {
-      src: 'https://s27.postimg.cc/f39ejgmqb/listing-2.jpg',
-      w: 1024,
-      h: 679
-    },
-    {
-      src: 'https://s27.postimg.cc/jcsbgxwvn/listing-3.jpg',
-      w: 1024,
-      h: 679
-    }
-  ];
 
-  // define options (if needed)
-  var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 2 // start at first slide
-  };
 
-  // Initializes and opens PhotoSwipe
-  var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-  gallery.init();
+// 
+// var startSlideshow = function() {
+//
+//   var pswpElement = document.querySelectorAll('.pswp')[0];
+//
+//   // console.log(pswpElement.innerHTML);
+//
+//   // var arr = $(".tile-wall").toArray();
+//   //
+//   // console.log(arr);
+//
+//   // build items array
+//   var items = [
+//     {
+//       src: 'https://s27.postimg.cc/g2so1q503/listing-0.jpg',
+//       w: 1024,
+//       h: 679
+//     },
+//     {
+//       src: 'https://s28.postimg.cc/j7pcb7r25/listing-1.jpg',
+//       w: 1024,
+//       h: 679
+//     },
+//     {
+//       src: 'https://s27.postimg.cc/f39ejgmqb/listing-2.jpg',
+//       w: 1024,
+//       h: 679
+//     },
+//     {
+//       src: 'https://s27.postimg.cc/jcsbgxwvn/listing-3.jpg',
+//       w: 1024,
+//       h: 679
+//     }
+//   ];
+//
+//   // define options (if needed)
+//   var options = {
+//     // optionName: 'option value'
+//     // for example:
+//     index: 2 // start at first slide
+//   };
+//
+//   // Initializes and opens PhotoSwipe
+//   var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+//   gallery.init();
+// }
+//
+// $(function() {
+//
+//   $('.js-slideshow').on('click', function() {
+//     startSlideshow();
+//   });
+//
+// });
+//
+// $(function() {
+//
+//   $('.tile-lg').on('click', function() {
+//     startSlideshow();
+//   });
+//
+// });
+//
+// $(function() {
+//
+//   $('.tile-sm').on('click', function() {
+//     startSlideshow();
+//   });
+//
+// });
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
 }
-
-$(function() {
-
-  $('.js-slideshow').on('click', function() {
-    startSlideshow();
-  });
-
-});
---
+++ public/js/test-gallery.js
@@ -0,0 +1,4 @@
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+
+imgs.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
