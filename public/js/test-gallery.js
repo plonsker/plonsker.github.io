be37203a95d24739b471b51a8a6cf6fbc80b9aa6
plonsker <joeplonsker@gmail.com>
2020-12-09 19:50:24 -0500

    finish test gallery

--- public/js/test-gallery.js
+++ public/js/test-gallery.js
@@ -1,4 +1,37 @@
-const current = document.querySelector('#current');
+const current = document.querySelectorAll('.current');
 const imgs = document.querySelectorAll('.imgs img');
+const imgParent = document.querySelectorAll('.imgs');
+const opacity = 0.4;
 
-imgs.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+
+
+
+$.each($(".current"), function(index, value){
+    var num = index + 1;
+    $(value).attr("id", + num);
+});
+
+$.each($(".imgs"), function(index, value){
+    var num = index + 1;
+    $(value).attr("id", + num);
+});
+
+
+
+function imgClick(e) {
+  imgs.forEach(img => (img.style.opacity = 1));
+
+  console.log(this.parentNode.id)
+
+  current[this.parentNode.id - 1].src = e.target.src;
+
+  current[this.parentNode.id - 1].classList.add('fade-in');
+
+  setTimeout(() => current[this.parentNode.id - 1].classList.remove('fade-in'), 300);
+
+  e.target.style.opacity = opacity;
+}
